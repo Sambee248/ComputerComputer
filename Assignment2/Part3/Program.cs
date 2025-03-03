59e1aaa543c7393777ffcd6d4ada490a2a14bd95
Sambee248 <2482251471@qq.com>
2025-03-03 17:58:06 +0800

    Add files via upload

---
+++ Assignment2/Part3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program{
+    static void Main(){
+        //2~100内的素数
+        int[] primeNumber = new int[50];
+        int order = 0;  //序号
+        for(int i = 2; i <= 100 ; i++){
+            for(int j = 2;j < i; j++){
+                if( i % j == 0) break;   //除得尽直接进入下一轮循环
+                if( j == i-1 ){
+                    primeNumber[order] = i;
+                    order ++;
+                }
+            }
+        };
+        Console.WriteLine("2~100之间的素数有:");
+        for(int m = 0;m < order; m++){
+            Console.Write(primeNumber[m] + " ");
+        }
+    }
+}
